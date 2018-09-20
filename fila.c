7b071b359861e183717ffe7123f92f482e7d0d60
Suniaster <thibousbr@gmail.com>
2018-09-20 12:32:45 -0300

    Trocados nomes dos arquivos

--- fila.c
+++ fila.c
@@ -2,21 +2,38 @@
 #include <stdlib.h> //Malloc() e free()
 #include "headers.h"
 
+Fila* CriaFiladeCliente(){
+    Fila* f;
+    f=(Fila*)malloc(sizeof(Fila));
+    if(f==NULL)exit(1);
+    f->Head=(Lista*)malloc(sizeof(Lista));
+    f->Tail=f->Head;
+    f->tamanho=0;
+    return f;
+}
 
+void InsereCliente(Fila* f, Cliente* C){
+    f->Tail->prox=(Lista*)malloc(sizeof(Lista));;
+    f->Tail->prox->info=(Cliente*)C;
+    f->Tail=f->Tail->prox;
+    f->tamanho++;
+}
 
-int main(int argc, char** argv){
-    if(argc != 2){
-        printf("Quantidade de argumentos invalida\n");
-        exit(1);
+Cliente* TiraCliente(Fila *f){
+    Lista* aux;
+    Cliente *Retorno;
+    aux=f->Head;
+    f->Head=aux->prox;
+
+    Retorno = (Cliente*)f->Head->info;
+    free(aux);
+    f->tamanho--;
+    return Retorno;
+} 
+
+int FVazia(Fila* f){
+    if(f->Head==f->Tail){
+        return 1;
     }
-    Fila *PrimeiraFila;
-    Cliente *C;
-    PrimeiraFila=LeDados(argv[1]);
-    int a = FVazia(PrimeiraFila);
-    while(!FVazia(PrimeiraFila)){
-        C=TiraCliente(PrimeiraFila);
-        printf("%d %d\n",C->cond, C->idade);
-        free(C);
-    }
-    return 0;
+    else return 0;
 }
