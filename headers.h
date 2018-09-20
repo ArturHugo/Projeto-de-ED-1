02424d52eea11e8ca89df5de3e72c30566de4005
Suniaster <thibousbr@gmail.com>
2018-09-19 23:02:55 -0300

    Criando prototipos das Filas e Clientes

---
+++ headers.h
@@ -0,0 +1,20 @@
+#ifndef Header
+#define Header
+
+typedef struct Pessoa{
+    int tempo_chegada;
+    int idade;
+    int serv; //Serviço desejado pelo Cliente
+    int cond; //Condição física do Cliente
+
+    //Faltam algumas caracteristicas
+    //Exemplo:
+    //  int tempo_espera;
+    //  int prioridade;
+    //  int guiche; 
+}Cliente;
+
+
+
+
+#endif
