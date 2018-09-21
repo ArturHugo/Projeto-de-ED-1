44be30b092c1a33315cad5296d2d269cc0e60eb8
ArturHugo <arturpereira@cjr.org.br>
2018-09-21 12:27:13 -0300

    Modifica main.c para testar LeDados e InserirNaLista.

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "headers.h"
 
-int main(){
+//A estrutura atual da main esta voltada ao teste de LeDados
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Erro - numero de argumentos incorreto.\n");
+        exit(1);
+    }
 
+    LeDados(argv[1]);
 
     return 0;
 }
