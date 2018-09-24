d0faa962e8d7f5ab11516eab25f624cafb590962
Suniaster <thibousbr@gmail.com>
2018-09-23 21:36:48 -0300

    Organizads funções dentro do Headers E todas foram comentadas dentro do Headers

--- headers.h
+++ headers.h
@@ -1,18 +1,20 @@
 #ifndef Header
 #define Header
 
+/* Lista Generalizada usando ponteiro para void como forma de apontar para dados */
 typedef struct Lista{
     struct Lista* prox;
-    struct Lista* ant;
     void* info;
 }Lista;
 
+/* Fila Implementada usando ponteiros para Lista, junto com seus demais metodos Apresentados abaixo*/
 typedef struct Fila{
     Lista* Head;
     Lista* Tail;
     int tamanho;
 }Fila;
 
+/* Estrutura de Dados Para armazenar informações sobre os clientes da Clinica */
 typedef struct Pessoa{
     int tempo_chegada;      //Tempo de chegada na clinica
     int tempo_inicio;       //Tempo de inicio do atendimento
@@ -27,18 +29,20 @@ typedef struct Pessoa{
     int guiche;             //Numero do guiche para onde a pessoa foi encaminhada
 }Cliente;
 
+/* Estrutura de Dados para armazenar informações Gerais do programa */
 typedef struct InformacaoGlobal{
     float clientePorTempo;  //Numero de clientes atendidos por unidade de tempo
     float tempoMedio;       //Tempo medio de espera dos clientes nas filas
 }InfoGlobal;
 
+/* Estrutura de Dados para armazenar informações sobre cada Guiche da Clinica */
 typedef struct Guiche{
     int serv;              //Servico do guiche
     int num;               //Numero do guiche
     int tempoUltimoAtendimento;
 }Guiche;
-/* Funções de entrada e saida de dados */                                    //Retorna um numero aleatorio entre valor minimo e maximo
-    Fila* LeDadosClientes(char *Filename);                                                  //Lê os dados dos CLIENTES e retorna uma fila sem organização
+/* Funções de entrada e saida de dados */                                          
+    Fila* LeDadosClientes(char *Filename);                                         //Lê os dados dos CLIENTES e retorna uma fila sem organização
     Fila* LeDadosGuiches(char *Filename);
         /* Cria Arquivos Com Dados de clientes */
     void GeraAleatoriosClientes(char* ArquivoSaida, int quantidade);               //Coloca dados de uma quantidade de clientes com caracteristicas aleatorias no arquivo de saida  
@@ -49,19 +53,25 @@ typedef struct Guiche{
     void EscreveRelatorio(char* ArquivoSaida, InfoGlobal Global, Fila *Clientes);  //Cria o relatorio com as informações gerais do programa
 /* Fim das funções de entrada e saida */
 
-float CalculaTempodeEsperaMedio(Fila *f);
-void InsereClienteTempo(Fila* f, Cliente* c);
-void InsereClientePrioridade(Fila* f, Cliente* c);
-Fila** Cria5Filas();
-Fila** Separa5Guiches(Fila* Principal); //Separa as filas dos guiches
-Fila* CriaFilaVazia();
-void FreeFila(Fila* f);
-void InsereCliente(Fila* f, Cliente* C);
-Cliente* TiraCliente(Fila* f);
-int FVazia(Fila* f);
-Cliente* CriaCliente();
-void PassaDados(Cliente* C,int tempo_chegada,int idade,int serv, int cond);
-void InserirNaFila(Fila* f, void* elemento);
-void* TiraElementoDaFila(Fila* f);
+
+/* Funções de Manipulação de Filas */
+Fila** Cria5Filas();                                                                //Aloca um vetor de 5 filas
+Fila** Separa5Guiches(Fila* Principal);                                             //Separa as filas dos guiches
+Fila* CriaFilaVazia();                                                              //Cria uma fila e retorna ponteiro para ela
+void FreeFila(Fila* f);                                                             //Libera os dados da fila e de seus elemento
+void InsereCliente(Fila* f, Cliente* C);                                            //Insere Cliente ma Fila
+void InsereClienteTempo(Fila* f, Cliente* c);                                       //Insere Cliente de acordo com seu tempo de chegada (ordem decrescente)  
+void InsereClientePrioridade(Fila* f, Cliente* c);                                  //Insere Cliente de acordo com sua prioridade (ordem decrescente)
+void InserirNaFila(Fila* f, void* elemento);                                        //Insere elemento no Final da Fila
+void* TiraElementoDaFila(Fila* f);                                                  //Tira elemento qualquer da fila, retornando o ponteiro para ele
+Cliente* TiraCliente(Fila* f);                                                      //Tira primeiro Cliente da Fila retornando ponteiro para Cliente
+int FVazia(Fila* f);                                                                //Retorna 1 caso a Fila esteja vazia e 0 caso contrario
+/* Fim das funções de Fila */
+
+/* Funções de Manipulação de Clientes */
+float CalculaTempodeEsperaMedio(Fila *f);                                           //Usa os dados da fila de clientes para calcular o tempo medio de espera
+Cliente* CriaCliente();                                                             //Retorna espaço alocado para um Cliente
+void PassaDados(Cliente* C,int tempo_chegada,int idade,int serv, int cond);         //Usa dados lidos para adicionar às informações do Cliente
+/* Fim das Funções de Clientes */
 
 #endif
